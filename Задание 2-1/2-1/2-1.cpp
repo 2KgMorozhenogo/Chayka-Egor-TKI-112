b56a668603785330663f04e2be43d7278ab9ddb7
Egor-Always-AFK <shokolokill@gmail.com>
2021-04-29 22:29:44 +0300

    Написана программа для задачи 2-1
    1) 2-1.sln
    2) 2-1.cpp
    3) 2-1.vcxproj
    4) 2-1.vcxproj.filters

---
+++ Задание 2-1/2-1/2-1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a, b, c, f;
+	cout << "Введите а - ";
+	cin >> a;
+	cout << "Введите b - ";
+	cin >> b;
+	cout << "Выберите алгоритм ((1) - с 3 переменными; (2) - без) - ";
+	cin >> f;
+	if (f == 1) {
+		f = a;
+		a = b;
+		b = f;
+		cout << "Введите а - " << a << '\n';
+		cout << "Введите b - " << b << '\n';
+	}
+	else {
+		swap(a, b);
+		cout << "Введите а - " << a << '\n';
+		cout << "Введите b - " << b << '\n';
+	}
+}
