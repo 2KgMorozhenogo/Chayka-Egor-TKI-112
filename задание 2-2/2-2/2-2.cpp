5cba49bfe9ad72e97b34bdad507d516702fca026
Egor-Always-AFK <shokolokill@gmail.com>
2021-05-19 12:03:21 +0300

    Решение задачи 2-2
    Добавлены файлы:
    1)2-2.sln
    2)2-2.cpp
    3)2-2.vcxproj
    4)2-2.vcxproj.filters

---
+++ задание 2-2/2-2/2-2.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int a = 2.5; // инициализация константы
+double x, y;   // инициализация переменных
+
+void input() //функция ввода
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите x = "; 
+	cin >> x;
+}
+
+void solution() //функция поиска решений
+{
+	if (x > a) 
+	{
+		y = (x * (pow(sin(x), 2)));
+		std::cout << "Значение y равно " << y << endl;
+	}
+	else
+	{
+		y = (x * (pow(cos(x), 2)));
+		std::cout << "Значение y равно " << y << endl;
+	}
+}
+int main()
+{
+	input();
+
+	solution();
+
+	return 0;
+}
