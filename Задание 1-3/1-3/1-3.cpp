99ad312fb8c53902719140fdcc8d56d6ee59166d
Egor-Always-AFK <shokolokill@gmail.com>
2021-05-23 15:37:48 +0300

    Изменено решение и добавлена документация к задаче 1-3

--- Задание 1-3/1-3/1-3.cpp
+++ Задание 1-3/1-3/1-3.cpp
@@ -1,18 +1,40 @@
-﻿#include <cmath>
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
+
+/**
+* \brief Вычисление давления
+* \param m - масса
+* \param s - площадь
+* \param g - ускорение свободного падения
+*/
+void davlenie(double s, double m, double const g);
+
+
+void davlenie(double s, double m, double const g)
+{
+	std::cout << "Давление равно - " << (m * g) / s << endl;
+}
+
+/**
+* \brief точка входа в программу
+* \param m - масса
+* \param s - площадь
+* \param g - ускорение свободного падения
+* \return Код ошибки(0 - успех)
+*/
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double m, S, P;
-	const double g = 9.80665;
-	cout << "Введите масу объекта - ";
-	cin >> m;
-	cout << '\n';
-	cout << "Введите площадь - ";
-	cin >> S;
-	cout << '\n';
-	P = (m * g) / S;
-	cout << "Давление = " << P;
+	double s, m;
+	double const g = 9.8;
+	std::cout << "Введите S - ";
+	std::cin >> s;
+	std::cout << "Введите m - ";
+	std::cin >> m;;
+
+	davlenie(s, m, g);
 }
+
+
+
