679c91c99179ac533d6f72d9c754577fef443f51
Egor-Always-AFK <shokolokill@gmail.com>
2021-05-23 16:02:27 +0300

    Добавлена документация к задаче 1-2

--- Задание 1-2/1-2/1-2.cpp
+++ Задание 1-2/1-2/1-2.cpp
@@ -2,8 +2,25 @@
 #include <math.h>
 
 using namespace std;
+/**
+* \brief Вычисление расстояния между двумя точками с заданными координтами 
+* \param double x1 - координата x первой точки
+* \param double y1 - координата y первой точки
+* \param double x2 - координата x второй точки
+* \param double y1 - координата y второй точки
+* \return расстояние между двумя точками
+*/
 double GetDistance(const double x1, const double y1, const double x2, const double y2);
 
+
+/**
+* \brief Точка входа в программу
+* \param double x1 - координата x первой точки
+* \param double y1 - координата y первой точки
+* \param double x2 - координата x второй точки
+* \param double y1 - координата y второй точки
+* \return Код ошибки(0 - успех)
+*/
 int main()
 {
 	double x1, y1;
