df05178fbb6948fd69b6aa65f8be90ec2d04dbb9
Egor-Always-AFK <52970978+Egor-Always-AFK@users.noreply.github.com>
2021-05-24 11:48:10 +0300

    Update 2-2.cpp

--- задание 2-2/2-2/2-2.cpp
+++ задание 2-2/2-2/2-2.cpp
@@ -10,7 +10,7 @@ double x, y;   // инициализация переменных
 void input() //функция ввода
 {
 	setlocale(LC_ALL, "Russian");
-	std::cout << "Введите x = "; 
+	cout << "Введите x = "; 
 	cin >> x;
 }
 
@@ -19,12 +19,12 @@ void solution() //функция поиска решений
 	if (x > a) 
 	{
 		y = (x * (pow(sin(x), 2)));
-		std::cout << "Значение y равно " << y << endl;
+		cout << "Значение y равно " << y << endl;
 	}
 	else
 	{
 		y = (x * (pow(cos(x), 2)));
-		std::cout << "Значение y равно " << y << endl;
+		cout << "Значение y равно " << y << endl;
 	}
 }
 int main()
